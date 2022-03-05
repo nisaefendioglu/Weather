f047cc21dfc148fda55692dfae791d2b13aed1f6
Nisa Efendioğlu <nisaefendioglu0@gmail.com>
2022-03-05 15:47:12 +0300

    Completed 🥳

--- app/src/main/java/com/nisaefendioglu/weather/view/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/weather/view/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.nisaefendioglu.weather.view
 
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -11,6 +13,12 @@ import com.bumptech.glide.Glide
 import com.nisaefendioglu.weather.R
 import com.nisaefendioglu.weather.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
 
 private const val TAG = "MainActivity"
 
@@ -21,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var GET: SharedPreferences
     private lateinit var SET: SharedPreferences.Editor
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,9 +39,24 @@ class MainActivity : AppCompatActivity() {
 
         viewmodel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
-        var cName = GET.getString("cityName", "bingöl")?.toLowerCase()
+        var cName = GET.getString("cityName", "istanbul")?.toLowerCase()
         edt_city_name.setText(cName)
         viewmodel.refreshData(cName!!)
+
+
+
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        val dateTime = LocalDateTime.of(year, month, day, hour, minute)
+        val formatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        updated_at.setText(dateTime.format(formatter))
+
+
 
         getLiveData()
 
@@ -64,14 +88,16 @@ class MainActivity : AppCompatActivity() {
             data?.let {
                 ll_data.visibility = View.VISIBLE
 
-                tv_city_code.text = data.sys.country.toString()
-                tv_city_name.text = data.name.toString()
+                tv_city_code.text = data.sys.country
+                tv_city_name.text = data.name
+                address.text = data.name
 
                 Glide.with(this)
                     .load("https://openweathermap.org/img/wn/" + data.weather.get(0).icon + "@2x.png")
                     .into(img_weather_pictures)
 
-                tv_degree.text = data.main.temp.toString() + "°C"
+                var tempInt = data.main.temp.toInt();
+                tv_degree.text = tempInt.toString() + "°C"
 
                 tv_humidity.text = data.main.humidity.toString() + "%"
                 tv_wind_speed.text = data.wind.speed.toString()
